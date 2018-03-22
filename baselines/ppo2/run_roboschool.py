9951ba75a27010e098d1f0ff28719bb61ba95290
Marin Vlastelica Pogancic <marin.vlastelicap@gmail.com>
2018-03-22 13:43:00 +0100

    Added commandline arguments

--- baselines/ppo2/run_roboschool.py
+++ baselines/ppo2/run_roboschool.py
@@ -4,7 +4,23 @@ from baselines.common.cmd_util import roboschool_arg_parser
 from baselines import bench, logger
 import roboschool
 
-def train(env_id, num_timesteps, seed):
+    
+
+
+def main():
+    parser = roboschool_arg_parser()
+    parser.add_argument("--nsteps", help="Number of steps for advantage estimation", type=int, default=2048)
+    parser.add_argument("--noptsteps", help="Number of optimisation steps", default=10, type=int)
+    parser.add_argument("--entcoef", help="Entropy coefficient", default=0.0, type=float)
+    parser.add_argument("--mbatches", help="Number of minibatches", default=32, type=int)
+    parser.add_argument("--horizon", help="Time horizon", default=1000000, type=int)
+    parser.add_argument("--lmda", help="Lambda", default=0.95, type=float)
+    parser.add_argument("--gamma", help="Reward discount factor", default=0.99, type=float)
+
+
+    args = parser.parse_args()
+    logger.configure()
+
     from baselines.common import set_global_seeds
     from baselines.common.vec_env.vec_normalize import VecNormalize
     from baselines.ppo2 import ppo2
@@ -18,26 +34,20 @@ def train(env_id, num_timesteps, seed):
                             inter_op_parallelism_threads=ncpu)
     tf.Session(config=config).__enter__()
     def make_env():
-        env = gym.make(env_id)
+        env = gym.make(args.env)
         env = bench.Monitor(env, logger.get_dir())
         return env
     env = DummyVecEnv([make_env])
     env = VecNormalize(env)
 
-    set_global_seeds(seed)
+    set_global_seeds(args.seed)
     policy = MlpPolicy
-    ppo2.learn(policy=policy, env=env, nsteps=2048, nminibatches=32,
-        lam=0.95, gamma=0.99, noptepochs=10, log_interval=1,
-        ent_coef=0.0,
+    ppo2.learn(policy=policy, env=env, nsteps=args.nsteps, nminibatches=args.mbatches,
+        lam=args.lmda, gamma=args.gamma, noptepochs=args.noptsteps, log_interval=1,
+        ent_coef=args.entcoef,
         lr=3e-4,
         cliprange=0.2,
-        total_timesteps=num_timesteps)
-
-
-def main():
-    args = roboschool_arg_parser().parse_args()
-    logger.configure()
-    train(args.env, num_timesteps=args.num_timesteps, seed=args.seed)
+        total_timesteps=args.horizon)
 
 
 if __name__ == '__main__':
